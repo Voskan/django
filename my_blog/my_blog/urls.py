a41c2f5a7893f1bd9caa7e7beaa6cda3957f0aef
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2022-06-05 10:46:09 +0400

    update template

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('shop/', include('shop.urls'))
+    path('', include('shop.urls'))
 ]
 
 if settings.DEBUG:
--- my_blog/shop/views.py
+++ my_blog/shop/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request, 'shop/index.html')
+  d = [
+    {
+      "id": 1,
+      "name": "MacBook ipsum dolor sit amet consectetur.",
+      "price": 2560,
+      "count": 12,
+      "image": "hp.webp",
+      "categories": ["Electronics", "Computers", "Tools"]
+    },
+    {
+      "id": 2,
+      "name": "HP ipsum dolor sit amet consectetur.",
+      "price": 1500,
+      "count": 10,
+      "image": "zenbook.webp",
+      "categories": ["Computers", "Tools"]
+    },
+    {
+      "id": 3,
+      "name": "DELL ipsum dolor sit amet consectetur.",
+      "price": 1200,
+      "count": 13,
+      "image": "hp.webp",
+      "categories": ["Electronics", "Tools"]
+    },
+    {
+      "id": 4,
+      "name": "ASUS ipsum dolor sit amet.",
+      "price": 1100,
+      "count": 18,
+      "image": "zenbook.webp",
+      "categories": ["Electronics", "Computers"]
+    }
+  ]
+
+  return render(request, 'shop/index.html', {
+    "products": d,
+    "hello": "Barev"
+  })
