8f2a88c66a5c976adfd665a5dbe0e2ac0cced3eb
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2022-06-05 12:40:08 +0400

    model

--- my_blog/shop/models.py
+++ my_blog/shop/models.py
@@ -1,3 +1,15 @@
+from itertools import count
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=80)
+  price = models.IntegerField()
+  count = models.SmallIntegerField()
+  availability = models.BooleanField(default=True)
+  description = models.TextField()
+  processor = models.CharField(max_length=20)
+  cpu = models.CharField(max_length=20)
+  processor_cache = models.SmallIntegerField()
+  screen_type = models.CharField(max_length=20)
+  screen_size = models.SmallIntegerField()
+  brand = models.CharField(max_length=20)
